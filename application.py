b46ca41bba7955e01001872bcc3df886dda887f8
Veronica Meyer <eveweird@eve-mac.local>
2021-07-29 07:39:29 +1000

    init;

--- application.py
+++ application.py
@@ -1,21 +1,19 @@
 from flask import Flask, render_template
-import pyodbc
+import pymysql.cursors
+
+db = pymysql.connect(host='dci-database.capioxzatswy.ap-southeast-2.rds.amazonaws.com',
+                             user='admin',
+                             password='WH4eO1nkUbsoD8a0iUXi',  
+                             db="sys",
+                             charset='utf8mb4',
+                             )
+
+cursor = db.cursor()
+query = "SELECT * FROM sys.DCIDB;"
+cursor.execute(query)
+data = cursor.fetchall()
 
 application = Flask(__name__, template_folder='template')
-
-
-
-server = 'database-1.capioxzatswy.ap-southeast-2.rds.amazonaws.com'
-database = 'dci-database'
-username = 'admin'
-password = 'WH4eO1nkUbsoD8a0iUXi'
-driver = '{ODBC Driver 17 for SQL Server}'
-
-with pyodbc.connect(
-        'DRIVER=' + driver + ';SERVER=' + server + ';PORT=1433;DATABASE=' + database + ';UID=' + username + ';PWD=' + password) as conn:
-    with conn.cursor() as cursor:
-        cursor.execute("SELECT * FROM DCIDB")
-        data = cursor.fetchall()
 
 @application.route('/', methods=['GET', 'POST'])
 def hello_world():
