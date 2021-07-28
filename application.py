fa9e83da94b0d19b11cffa95b325438e380c1c1b
Veronica Meyer <eveweird@eve-mac.local>
2021-07-29 01:27:23 +1000

    init

--- application.py
+++ application.py
@@ -1,7 +1,19 @@
 from flask import Flask, render_template
+import pymysql
+import re
 
 application = Flask(__name__, template_folder='template')
 
+host='dci-database.capioxzatswy.ap-southeast-2.rds.amazonaws.com'
+user = 'admin'
+password = 'WH4eO1nkUbsoD8a0iUXi'
+db = 'dci-database'
+
+con = pymysql.connect(host=host,user=user,password=password,db=db, use_unicode=True, charset='utf8')
+       
+cur = con.cursor()
+cur.execute("SELECT * FROM dataset")
+data = cur.fetchall()
 
 @application.route('/', methods=['GET', 'POST'])
 def hello_world():
@@ -9,4 +21,4 @@ def hello_world():
 
 @application.route('/database/', methods=['GET', 'POST'])
 def about():
-    return render_template('database.html')
+    return render_template('database.html', data=data)
