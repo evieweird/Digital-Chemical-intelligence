df735f1800e9dbd8975b0438562ef2a4970bafcc
Veronica Meyer <eveweird@eve-mac.local>
2021-07-29 12:09:47 +1000

    init

--- application.py
+++ application.py
@@ -4,13 +4,10 @@ from flask import Flask, render_template, Markup
 application = Flask(__name__, template_folder='template')
 
 
-
-
 @application.route('/', methods=['GET', 'POST'])
 def hello_world():
     return render_template('index.html')
 
 @application.route('/database/', methods=['GET', 'POST'])
 def about():
-    
     return render_template('database.html', data="hi")
